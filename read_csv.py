20e5c8f1bfed7e49902a907b175e988275986a11
monah-net <SolOleg86@gmail.com>
2021-05-13 15:47:38 +0300

    add read excel and json

--- read_csv.py
+++ read_csv.py
@@ -3,3 +3,11 @@ import pandas as pd
 
 def read_csv(file):
     return pd.read_csv(file, delimiter=';')
+
+
+def read_excel(file):
+    return pd.read_excel(file, sheet_name='Input')
+
+
+def read_json(file):
+    return pd.read_json(file)
