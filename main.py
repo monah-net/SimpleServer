5a1c3f2822841346754c61a192112b506cce583c
blypovetskyi <blypovetskyi@gmail.com>
2021-05-16 13:49:37 +0300

    console input and logging provided

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 #! /usr/bin/env python3
-from utils import app
+from file_service import create_file, read_file, delete_file, get_metadata
+from utils import InvalidOption
+import argparse
+import logging
+
+
+def commandline_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action', type=str, help='Action')
+    parser.add_argument('-fn', '--file_name', type=str, help='File name')
+    parser.add_argument('-fe', '--file_extension', type=str, default='pkl', help='File extension')
+
+    return parser
+
+
+def app():
+    args = commandline_parser().parse_args()
+    try:
+        choice = args.action
+        if choice == 'create':
+            content = input('Enter file content or press <Enter> to continue')
+            create_file(args.file_extension, content)
+        elif choice == 'read':
+            if args.file_name is None:
+                raise InvalidOption
+            read_file(args.file_name)
+        elif choice == 'delete':
+            if args.file_name is None:
+                raise InvalidOption
+            print(args.file_name)
+            delete_file(args.file_name)
+        elif choice == 'metadata':
+            if args.file_name is None:
+                raise InvalidOption
+            get_metadata(args.file_name)
+        else:
+            raise InvalidOption
+    except InvalidOption:
+        logging.error('Invalid option is chosen.')
+    except FileNotFoundError:
+        logging.error('File is not found.')
+
 
 if __name__ == '__main__':
     app()
