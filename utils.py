7c948d608c551c90e908904a82aaab95054c339b
blypovetskyi <blypovetskyi@gmail.com>
2021-05-17 14:24:46 +0300

    console input and logging provided

--- utils.py
+++ utils.py
@@ -6,10 +6,22 @@ LETTERS_DIGITS = string.ascii_letters + string.digits
 
 
 def generate_name() -> str:
+    """
+    Generate random name of the file composed of letters and digits.
+
+    Returns:
+        File name.
+    """
     return ''.join(random.choice(LETTERS_DIGITS) for _ in range(FILE_NAME_LENGTH))
 
 
 def enter_content() -> str:
+    """
+    Show message to input file content.
+
+    Returns:
+         file content
+    """
     content = input("Please enter file content or press <Enter> to continue.\n")
     return content
 
