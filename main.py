ff979b565c4d57e10c7de63bb7e15a4b176a43b4
monah-net <SolOleg86@gmail.com>
2021-05-24 01:05:03 +0300

    Main module, added functions for file uploading. Added index.html page

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 #! /usr/bin/env python3
+import os
+from werkzeug.utils import secure_filename, redirect
 from utils import app
+from flask import Flask, render_template, request, url_for
 
-if __name__ == '__main__':
-    app()
+app = Flask(__name__)
+UPLOAD_FOLDER = os.getcwd()
+ALLOWED_EXTENSIONS = set(['txt', 'xlsx', 'xls', 'csv'])
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template("index.html")
