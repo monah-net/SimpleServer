b8b98bf88d9a32d5e5b2bd1a31d26b4df5df268f
monah-net <SolOleg86@gmail.com>
2021-05-25 14:57:39 +0300

    Add start page, page for uploading, functions for uploading into main module

--- main.py
+++ main.py
@@ -2,15 +2,32 @@
 import os
 from werkzeug.utils import secure_filename, redirect
 from utils import app
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 UPLOAD_FOLDER = os.getcwd()
-ALLOWED_EXTENSIONS = set(['txt', 'xlsx', 'xls', 'csv'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = {'txt', 'xlsx', 'xls', 'csv'}
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///FileFolder.db'
+db = SQLAlchemy(app)
+
+
+# class File(db.Mode):
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/uploads")
+def upload():
+    return render_template("uploads.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 def allowed_file(filename):
@@ -18,13 +35,21 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/uploads', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['file']
+        file = request.files['filename']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return render_template("index.html")
+            return render_template('index.html')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
